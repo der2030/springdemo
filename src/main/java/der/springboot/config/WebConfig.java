c1c29bfa6be51ad2df52670848af887db4db513c
der2030 <derrickye@hotmail.com>
2018-01-09 19:31:07 +1300

    SpringBoot Demo V1.0

---
+++ src/main/java/der/springboot/config/WebConfig.java
@@ -0,0 +1,27 @@
+package der.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+
+/**
+* @FileName:WebConfig
+* @Description:
+* @Author: Derrick Ye
+*/
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+        resolver.setFallbackPageable(new PageRequest(0, 5));
+        argumentResolvers.add(resolver);
+        super.addArgumentResolvers(argumentResolvers);
+    }
+}
